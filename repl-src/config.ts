85b50b18992ebc89a976cfdd7eec0df961841191
Kenny F <kfrachelle123@gmail.com>
2019-06-27 18:02:15 +0800

    fix(repl): updated left menu options

--- repl-src/config.ts
+++ repl-src/config.ts
@@ -8,20 +8,20 @@ export const LEFT_MENU_CONFIG = [
     items: [
       { title: "Directives", value: "directives", selected: false },
       { title: "Index-based range", value: "ranges", selected: false },
-      { title: "Implied strict", value: "impliedStrict", selected: false },
-      { title: "JSX", value: "jsx", selected: false, disabled: true }, // grayed out
-      { title: "Line and column-based", value: "loc", selected: false },
+      { title: "Strict mode", value: "impliedStrict", selected: false },
+      { title: "JSX", value: "jsx", selected: false, disabled: true },
+      { title: "Line/column location", value: "loc", selected: false },
       { title: "Lexical", value: "lexical", selected: false },
-      { title: "Next", value: "next", selected: false },
-      { title: "Raw", value: "raw", selected: false },
-      { title: "Web compat", value: "webcompat", selected: false }
+      { title: "ESNext features", value: "next", selected: false },
+      { title: "Raw property on literal node", value: "raw", selected: false },
+      { title: "Web compability", value: "webcompat", selected: false }
     ]
   },
   {
     category: "Parsers",
     items: [
-      { title: "JavaScript", value: "js", selected: false },
-      { title: "TypeScript", value: "ts", selected: false, disabled: true } // grayed out
+      { title: "JavaScript", value: "js", selected: true },
+      { title: "TypeScript", value: "ts", selected: false, disabled: true }
     ]
   }
 ];
