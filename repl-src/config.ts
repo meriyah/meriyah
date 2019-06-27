4483b9defbaa13af122f7c23804d737ac76d5a15
Kenny F <kfrachelle123@gmail.com>
2019-06-27 21:10:57 +0800

    fix(repl): fixed config settings

--- repl-src/config.ts
+++ repl-src/config.ts
@@ -8,19 +8,19 @@ export const LEFT_MENU_CONFIG = [
     items: [
       { title: "Directives", value: "directives", selected: false },
       { title: "Index-based range", value: "ranges", selected: false },
-      { title: "Strict mode", value: "impliedStrict", selected: false },
       { title: "JSX", value: "jsx", selected: false, disabled: true },
       { title: "Line/column location", value: "loc", selected: false },
       { title: "Lexical scope tracking", value: "lexical", selected: false },
       { title: "ESNext features", value: "next", selected: false },
       { title: "Raw property", value: "raw", selected: false },
+      { title: "Strict mode", value: "impliedStrict", selected: false },
       { title: "Web compability", value: "webcompat", selected: false }
     ]
   },
   {
     category: "Parsers",
     items: [
-      { title: "JavaScript", value: "js", selected: true },
+      { title: "JavaScript", value: "js", selected: true, disabled: true },
       { title: "TypeScript", value: "ts", selected: false, disabled: true }
     ]
   }
