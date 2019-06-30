d8c6f9c68bb8a6af76d896149768b4bc3c2594a4
Ivan Orlov <ivan.orlov@zervant.com>
2019-06-30 11:50:03 +0200

    fix: Failing editor from localstorage

--- repl-src/App.tsx
+++ repl-src/App.tsx
@@ -5,16 +5,16 @@ import { Editor } from "./editor";
 import * as meriyah from "../meriyah.umd.js";
 
 import { Header } from "./Header";
-import { DEFAULT_CODE } from './config';
+import { DEFAULT_CODE } from "./config";
 const initialCode = `function foo(bar){
   return bar;
 }`;
 
-function saveToLocalStorage(str : string){
+function saveToLocalStorage(str: string) {
   localStorage.setItem("code", str);
 }
 
-function getCodeFromLocalStage(){
+function getCodeFromLocalStage() {
   return localStorage.getItem("code") || DEFAULT_CODE;
 }
 
@@ -22,10 +22,15 @@ export class App extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = {
-      jsCode: getCodeFromLocalStage(),
+      jsCode: "",
       settings: { module: true },
       menuOpened: true
     };
+  }
+
+  componentWillMount() {
+    this.setState({ jsCode: getCodeFromLocalStage() });
+    this.parse();
   }
   private jsonEditor;
   private tm;
@@ -42,7 +47,7 @@ export class App extends React.Component<any, any> {
         this.jsonEditor.setValue(JSON.stringify(out, null, 2));
       }
     } catch (e) {
-      this.jsonEditor.setValue("");
+      this.jsonEditor && this.jsonEditor.setValue("");
       this.setState({ error: e.message });
     }
   }
