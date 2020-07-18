5fa94bde361167790500cde8bf47fe2eb30271ac
Kenny F <kfrachelle123@gmail.com>
2020-07-18 21:34:48 +0800

    chore(all): v. 2.1.0

--- src/meriyah.ts
+++ src/meriyah.ts
@@ -26,4 +26,4 @@ export function parse(source: string, options?: Options): ESTree.Program {
 export { Options, ESTree };
 
 // Current version
-export const version = '1.9.15';
+export const version = '2.1.0';
