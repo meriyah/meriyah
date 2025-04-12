5f23bf06352eec72fb85bec5f0add16b57634b82
fisker <lionkay@gmail.com>
2025-04-12 14:16:33 +0800

    chore: refactor

--- scripts/bundle.mjs
+++ scripts/bundle.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { join } from 'node:path';
+import path from 'node:path';
 import fs from 'node:fs/promises';
 import { rollup } from 'rollup';
 import typescript2 from 'rollup-plugin-typescript2';
@@ -9,11 +9,11 @@ import * as ts from 'typescript';
 
 // eslint-disable-next-line n/no-unsupported-features/node-builtins
 const { dirname } = import.meta;
-const ENTRY = join(dirname, '../src/meriyah.ts');
-const TSCONFIG = join(dirname, '../tsconfig.json');
-const DIST = join(dirname, '../dist/');
+const ENTRY = path.join(dirname, '../src/meriyah.ts');
+const TSCONFIG = path.join(dirname, '../tsconfig.json');
+const DIST = path.join(dirname, '../dist/');
 
-function getRollupOutputOptions(format, minified) {
+function createRollupOutputOptions(format, minified) {
   let suffix = format === 'umd' ? '.umd' : '';
   suffix += minified ? '.min' : '';
   suffix += format === 'esm' ? '.mjs' : format === 'cjs' ? '.cjs' : '.js';
@@ -21,34 +21,41 @@ function getRollupOutputOptions(format, minified) {
   return {
     name: 'meriyah',
     format,
-    file: join(DIST, `meriyah${suffix}`)
+    file: path.join(DIST, `meriyah${suffix}`),
+    plugins: minified ? [terser()] : []
   };
 }
 
+function* getRollupOutputOptions() {
+  for (const format of ['esm', 'umd', 'cjs']) {
+    yield createRollupOutputOptions(format, false);
+
+    // CommonJS version don't need minify
+    if (format === 'cjs') {
+      continue;
+    }
+
+    // Minified
+    yield createRollupOutputOptions(format, true);
+  }
+}
+
+// Clean up `dist/`
 await fs.rm(DIST, { force: true, recursive: true });
 
-for (const { formats, minified } of [
-  { formats: ['esm', 'umd', 'cjs'], minified: false },
-  { formats: ['esm', 'umd'], minified: true }
-]) {
-  const bundle = await rollup({
-    input: ENTRY,
-    plugins: [
-      typescript2({
-        tsconfig: TSCONFIG,
-        typescript: ts,
-        clean: true
-      }),
-      json(),
-      ...(minified ? [terser()] : [])
-    ]
-  });
+const bundle = await rollup({
+  input: ENTRY,
+  plugins: [
+    typescript2({
+      tsconfig: TSCONFIG,
+      typescript: ts,
+      clean: true
+    }),
+    json()
+  ]
+});
 
-  await Promise.all(
-    formats.map((format) => {
-      const options = getRollupOutputOptions(format, minified);
-      console.log(`writing ${options.file}`);
-      return bundle.write(options);
-    })
-  );
+for (const options of getRollupOutputOptions()) {
+  console.log(`writing ${path.relative(process.cwd(), options.file)}`);
+  await bundle.write(options);
 }
