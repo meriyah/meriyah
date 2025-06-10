170719740db02c117f4b6c79e36464cb1dc0a05f
fisker Cheung <lionkay@gmail.com>
2025-06-10 15:25:55 +0800

    ci: enable test on Node.js v20 (#427)
    * ci: enable test on Node.js v20
    
    * Use npm run
    
    * Fix
    
    * parentPath

--- test/production/production-tests.ts
+++ test/production/production-tests.ts
@@ -1,20 +1,23 @@
 import { describe } from 'vitest';
 import fs from 'node:fs/promises';
-import url from 'node:url';
+import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 import * as t from 'node:assert/strict';
 
 const DIST_DIRECTORY = new URL('../../dist/', import.meta.url);
 
-const glob = (pattern: string): Promise<string[]> =>
-  // @ts-expect-error -- Safe
-  Array.fromAsync(fs.glob(pattern, { cwd: url.fileURLToPath(DIST_DIRECTORY) }));
-
 describe('Production test', async function () {
-  const files = [...(await glob('**/*'))].map((file) => file.replaceAll('\\', '/'));
+  const files = (await fs.readdir(DIST_DIRECTORY, { recursive: true, withFileTypes: true }))
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => {
+      const url = pathToFileURL(path.join(dirent.parentPath, dirent.name));
+      const relativePath = url.href.slice(DIST_DIRECTORY.href.length);
+      return { url, relativePath };
+    });
 
-  for (const file of files.filter((file) => !file.startsWith('types'))) {
-    it(file, async () => {
-      const meriyah = await import(new URL(file, DIST_DIRECTORY).href);
+  for (const { relativePath, url } of files.filter((file) => !file.relativePath.startsWith('types'))) {
+    it(relativePath, async () => {
+      const meriyah = await import(url.href);
 
       t.equal(typeof meriyah.version, 'string');
       t.doesNotThrow(() => {
@@ -27,13 +30,13 @@ describe('Production test', async function () {
 
   it('Package structure', async () => {
     // Should not emit `.d.ts.map` files
-    t.ok(files.every((file) => !file.endsWith('.d.ts.map')));
+    t.ok(files.every((file) => !file.relativePath.endsWith('.d.ts.map')));
 
-    const declarationFiles = files.filter((file) => file.endsWith('.d.ts'));
+    const declarationFiles = files.filter((file) => file.relativePath.endsWith('.d.ts'));
     // Should emit `.d.ts` files
     t.ok(declarationFiles.length > 0);
 
     // `.d.ts` files should be emitted in `types/` directory
-    t.ok(declarationFiles.every((file) => file.startsWith('types/')));
+    t.ok(declarationFiles.every((file) => file.relativePath.startsWith('types/')));
   });
 });
