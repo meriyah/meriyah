a8b9047259957b0dd0563c0316ffa0c289c0bd58
fisker <lionkay@gmail.com>
2025-06-12 04:43:47 +0800

    Another place

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 const IS_CI = Boolean(process.env.CI);
 const SHOULD_RUN_PRODUCTION_TEST = IS_CI || Boolean(process.env.PRODUCTION_TEST);
+const SHOULD_RUN_TEST262 = IS_CI || Boolean(process.env.SHOULD_RUN_TEST262);
 
 export default defineConfig({
   test: {
@@ -11,6 +12,8 @@ export default defineConfig({
       'test/test-utils.ts',
       // Skip production test on local by default
       ...(SHOULD_RUN_PRODUCTION_TEST ? [] : ['test/production/production-tests.ts']),
+      // Skip test 262 on local by default
+      ...(SHOULD_RUN_TEST262 ? [] : ['test/test262-parser-tests/parser-tests.ts']),
     ],
     watch: false,
     pool: 'threads',
