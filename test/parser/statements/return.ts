4cf59d5c7ced54ab3318fda13c4331ed6848d37b
fisker Cheung <lionkay@gmail.com>
2025-06-14 11:10:33 +0800

    test: add tests for CommonJS context (#445)

--- test/parser/statements/return.ts
+++ test/parser/statements/return.ts
@@ -6,6 +6,12 @@ describe('Statements - Return', () => {
     ['return', Context.None],
     ['() => return', Context.None],
     ['*() => {return}', Context.None],
+    // https://github.com/acornjs/acorn/issues/1376#issuecomment-2960924476
+    ['class X { static { return; } }', Context.InReturnContext],
+    // The following should be allowed in CommonJS
+    // https://github.com/acornjs/acorn/issues/1376#issuecomment-2960396571
+    ['new.target', Context.InReturnContext],
+    ['using foo = null', Context.InReturnContext | Context.OptionsNext],
   ]);
 
   pass('Statements - Return (pass)', [
