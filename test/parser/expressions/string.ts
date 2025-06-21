1bc22ce77ccae99f36a21f12ec84339c30c1528e
fisker <lionkay@gmail.com>
2025-06-22 06:16:32 +0800

    One more test

--- test/parser/expressions/string.ts
+++ test/parser/expressions/string.ts
@@ -1,6 +1,6 @@
 import { pass } from '../../test-utils';
 
 pass('StringLiteral', [
-  { code: String.raw`("\u2028");`, options: { loc: true, ranges: true } },
-  { code: String.raw`("\u2029");`, options: { loc: true, ranges: true } },
+  { code: '("\u2028");', options: { loc: true, ranges: true } },
+  { code: '("\u2029");', options: { loc: true, ranges: true } },
 ]);
