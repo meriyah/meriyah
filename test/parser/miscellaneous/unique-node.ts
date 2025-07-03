4d316388036081231f289ba6ebf9c3fc2e3255c4
fisker <lionkay@gmail.com>
2025-07-03 09:19:30 +0800

    Add tests

--- test/parser/miscellaneous/unique-node.ts
+++ test/parser/miscellaneous/unique-node.ts
@@ -2,7 +2,13 @@ import { describe, expect, it } from 'vitest';
 import type * as ESTree from '../../../src/estree';
 import { parseSource } from '../../../src/parser';
 
-const parseOption = { source: 'foo.js', uniqueKeyInPattern: true, loc: true, ranges: true };
+const parseOptions = {
+  source: 'foo.js',
+  uniqueKeyInPattern: true,
+  loc: true,
+  ranges: true,
+  module: true,
+};
 
 const assertClone = (nodeA: ESTree.Node, nodeB: ESTree.Node) => {
   // Same struct
@@ -13,7 +19,7 @@ const assertClone = (nodeA: ESTree.Node, nodeB: ESTree.Node) => {
   expect(nodeA.loc).not.toBe(nodeB.loc);
   expect(nodeA.loc!.start).not.toBe(nodeB.loc!.start);
   expect(nodeA.loc!.end).not.toBe(nodeB.loc!.end);
-  expect(nodeA.loc!.source).toBe(parseOption.source);
+  expect(nodeA.loc!.source).toBe(parseOptions.source);
 };
 
 const assertPropety = (property: ESTree.Property) => {
@@ -28,9 +34,19 @@ const assertAssignmentPattern = (property: ESTree.Property) => {
   assertClone(property.key, (property.value as ESTree.AssignmentPattern).left);
 };
 
+const assertImportSpecifier = (specifier: ESTree.ImportSpecifier) => {
+  expect(specifier.type).toBe('ImportSpecifier');
+  assertClone(specifier.local, specifier.imported);
+};
+
+const assertExportSpecifier = (specifier: ESTree.ExportSpecifier) => {
+  expect(specifier.type).toBe('ExportSpecifier');
+  assertClone(specifier.local, specifier.exported);
+};
+
 describe('Unique node', () => {
   it('ObjectExpression', () => {
-    const program = parseSource('({a})', parseOption);
+    const program = parseSource('({a})', parseOptions);
     const expression = program.body[0] as ESTree.ExpressionStatement;
     const object = expression.expression as ESTree.ObjectExpression;
     const property = object.properties[0] as ESTree.Property;
@@ -38,7 +54,7 @@ describe('Unique node', () => {
   });
 
   it('ObjectPattern', () => {
-    const program = parseSource('const {a} = {}', parseOption);
+    const program = parseSource('const {a} = {}', parseOptions);
     const declaration = program.body[0] as ESTree.VariableDeclaration;
     const pattern = declaration.declarations[0].id as ESTree.ObjectPattern;
     const property = pattern.properties[0] as ESTree.Property;
@@ -46,10 +62,31 @@ describe('Unique node', () => {
   });
 
   it('AssignmentPattern', () => {
-    const program = parseSource('const {a = 1} = {}', parseOption);
+    const program = parseSource('const {a = 1} = {}', parseOptions);
     const declaration = program.body[0] as ESTree.VariableDeclaration;
     const pattern = declaration.declarations[0].id as ESTree.ObjectPattern;
     const property = pattern.properties[0] as ESTree.Property;
     assertAssignmentPattern(property);
+  });
+
+  it('ImportSpecifier', () => {
+    const program = parseSource('import {foo} from "foo"', parseOptions);
+    const declaration = program.body[0] as ESTree.ImportDeclaration;
+    const specifier = declaration.specifiers[0] as ESTree.ImportSpecifier;
+    assertImportSpecifier(specifier);
+  });
+
+  it('ExportSpecifier', () => {
+    const program = parseSource('export {foo} from "foo"', parseOptions);
+    const declaration = program.body[0] as ESTree.ExportNamedDeclaration;
+    const specifier = declaration.specifiers[0] as ESTree.ExportSpecifier;
+    assertExportSpecifier(specifier);
+  });
+
+  it('ExportSpecifier 2', () => {
+    const program = parseSource('export {"foo"} from "foo"', parseOptions);
+    const declaration = program.body[0] as ESTree.ExportNamedDeclaration;
+    const specifier = declaration.specifiers[0] as ESTree.ExportSpecifier;
+    assertExportSpecifier(specifier);
   });
 });
