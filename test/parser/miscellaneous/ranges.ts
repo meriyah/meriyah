53cff05929e1b708b0c798012db668fbf39cb4e2
DimaIT <dima@surfly.com>
2026-03-24 14:29:21 +0100

    test: add snapshot tests for ranges option variants

--- test/parser/miscellaneous/ranges.ts
+++ test/parser/miscellaneous/ranges.ts
@@ -336,4 +336,24 @@ describe('Miscellaneous - ranges', () => {
     { code: 'for (\n[x][0] in y);', options: { ranges: true, loc: true } },
     { code: 'for (\n[x][0] of y);', options: { ranges: true, loc: true } },
   ]);
+
+  pass("Miscellaneous - ranges: 'properties' (pass)", [
+    { code: 'var x = 1;', options: { ranges: 'properties' } },
+    { code: '`hello ${1}`', options: { ranges: 'properties' } },
+    { code: 'let [a, b] = [1, 2];', options: { ranges: 'properties' } },
+    { code: '({x: y = z = 0} = 1)', options: { ranges: 'properties' } },
+    { code: 'function f(a = 1) {}', options: { ranges: 'properties' } },
+    { code: '/* comment */ var x;', options: { ranges: 'properties' } },
+    { code: 'for (let x of [1]) {}', options: { ranges: 'properties' } },
+  ]);
+
+  pass("Miscellaneous - ranges: 'array' (pass)", [
+    { code: 'var x = 1;', options: { ranges: 'array' } },
+    { code: '`hello ${1}`', options: { ranges: 'array' } },
+    { code: 'let [a, b] = [1, 2];', options: { ranges: 'array' } },
+    { code: '({x: y = z = 0} = 1)', options: { ranges: 'array' } },
+    { code: 'function f(a = 1) {}', options: { ranges: 'array' } },
+    { code: '/* comment */ var x;', options: { ranges: 'array' } },
+    { code: 'for (let x of [1]) {}', options: { ranges: 'array' } },
+  ]);
 });
